b68e2d4bc8b0d1f60d0b58edc81650dbaf815f73
cd <tech.nocraterpdevelopers@gmail.com>
2025-08-06 00:37:13 +0100

    Created the Auth Service and installed the required Nudget package

---
+++ Mango.services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Mango.services.AuthAPI.Data;
+
+public class AppDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext
+{
+    public AppDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    
+
+    protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+       
+    }
+}
